89d4cec0565f757b7eb06d276eb636e2f96bf6cc
nobuyukikobayashi <59198478+nbr41to@users.noreply.github.com>
2024-10-24 22:10:19 +0900

    Update fetchers.ts

--- src/app/(general)/nextjs-15/cache/fetchers.ts
+++ src/app/(general)/nextjs-15/cache/fetchers.ts
@@ -1,13 +1,16 @@
 'use server';
 
 export const getTimeForceCache = async () => {
-  const response = await fetch('/api/get-time', {
-    cache: 'force-cache',
-    next: {
-      tags: ['getTimeForceCache'],
+  const response = await fetch(
+    'http://worldtimeapi.org/api/timezone/Asia/Tokyo',
+    {
+      cache: 'force-cache',
+      next: {
+        tags: ['getTimeForceCache'],
+      },
     },
-  });
+  );
   const data = await response.json();
 
-  return data.time;
+  return data.unixtime;
 };
