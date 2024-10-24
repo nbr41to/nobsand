fc146143b9a507a1d22a21d08bb179a91e3ed2ef
nobuyukikobayashi <59198478+nbr41to@users.noreply.github.com>
2024-10-24 22:01:21 +0900

    なんなん

--- src/app/(general)/nextjs-15/cache/page.tsx
+++ src/app/(general)/nextjs-15/cache/page.tsx
@@ -1,13 +1,9 @@
 import { TimeForceCache } from './_components/TimeForceCache';
 
 export default async function Page() {
-  const response = await fetch('http://localhost:3000/api/get-time');
-  const data = await response.json();
-
   return (
     <div>
       <h2>response cacheについて</h2>
-      <div>raw data: {data.time}</div>
       <div>
         <TimeForceCache />
       </div>
